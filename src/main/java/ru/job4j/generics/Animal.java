18270ada08c7cdca3f2a23448f76ab21842ca1f1
Admin <alimdzhanova.arina@gmail.com>
2026-05-25 21:48:52 +0300

    generics

---
+++ src/main/java/ru/job4j/generics/Animal.java
@@ -0,0 +1,9 @@
+package ru.job4j.generics;
+
+class Animal {
+    @Override
+    public String toString() {
+        return "Animal";
+    }
+}
+
---
+++ src/main/java/ru/job4j/generics/Predator.java
@@ -0,0 +1,8 @@
+package ru.job4j.generics;
+
+class Predator extends Animal {
+    @Override
+    public String toString() {
+        return "Predator";
+    }
+}
